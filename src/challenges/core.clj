c2d7f91e361b6e4a356aaf7871a18d16247cc7b6
calebgregory <calebgregory@gmail.com>
2015-09-29 15:02:37 -0500

    Adding [] to sum-of=multiples.. and fib, and sum of even fibs up to 4,000,000

--- src/challenges/core.clj
+++ src/challenges/core.clj
@@ -8,9 +8,21 @@
 
 (defn sum-of-multiples-of-3-5
   "Find sum of all multiples of 3 and 5 from 0 to 1000"
+  []
   (reduce +
           (filter
            #(or
              (zero? (mod % 3))
              (zero? (mod % 5)))
            (range 1000))))
+
+(defn fib
+  [a b]
+  (cons a (lazy-seq (fib b (+ b a)))))
+
+(defn sum-of-even-fib
+  "Take sum of even fib numbers in range [0,4000000]"
+  []
+  (reduce +
+          (filter even?
+           (take 34 (fib 0 1)))))
