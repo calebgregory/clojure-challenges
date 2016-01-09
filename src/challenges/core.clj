f891dad1027d7ea2f4de12b84566bbe616547743
calebgregory <calebgregory@gmail.com>
2016-01-09 17:24:15 -0600

    Using :use not :require, adding 'seven' function

--- src/challenges/core.clj
+++ src/challenges/core.clj
@@ -1,5 +1,5 @@
 (ns challenges.core
-  (:use [clojure.string :as str]))
+  (:use [clojure.string :only [capitalize join]]))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -202,20 +202,70 @@
 
 (defn accum
   [str]
-  (->> (map-indexed vector)
+  (->> (map-indexed vector str)
        (map #(repeat (inc (first %)) (second %)))
-       (map str/join)
-       (map str/capitalize)
-       (str/join #"-")))
+       (map join)
+       (map capitalize)
+       (join #"-")))
 
 (defn accum1
   [str]
   (let [enum (map-indexed vector str)]
-    (str/join \-
+    (join \-
           (map (fn [[i c]]
                  (->>
                   c
                   (repeat (inc i))
-                  str/join
-                  str/capitalize))
+                  join
+                  capitalize))
                enum))))
+
+;; A number m of the form 10x + y is divisible by 7 if and only if x −
+;; 2y is divisible by 7. In other words, subtract twice the last digit
+;; from the number formed by the remaining digits.
+
+;; Continue to do this until a number known to be divisible or not by
+;; 7 is obtained; you can stop when this number has at most 2 digits
+;; because you are supposed to know if a number of at most 2 digits is
+;; divisible by 7 or not.
+
+;; The original number is divisible by 7 if and only if the last
+;; number obtained using this procedure is divisible by7.
+
+;; Examples:
+
+;; 1 : m = 371 -> 37 − (2×1) -> 37 − 2 = 35
+;;  thus, since 35 is divisible by 7, 371 is divisible by 7.
+;;  The number of steps to get the result is 1.
+;; 2 : m = 1603 -> 160 - (2 x 3) -> 154 -> 15 - 8 = 7
+;;  and 7 is divisible by 7.
+;; 3 : m = 372 -> 37 − (2×2) -> 37 − 4 = 33
+;;  thus, since 33 is not divisible by 7, 372 is not divisible by 7.
+;;  The number of steps to get the result is 1.
+;; 4 : m = 477557101->47755708->4775554->477547->47740->4774->469->28
+;;  and 28 is divisible by 7, so is 477557101.
+;;  The number of steps is 7.
+
+;; Task:
+
+;; Your task is to return to the function seven(m) (m integer >= 0) an
+;; array (or a pair, depending on the language) of numbers, the first
+;; being the last number m with at most 2 digits obtained by your
+;; function (this last m will be divisible or not by 7), the second
+;; one being the number of steps to get the result.
+
+(defn num-digits
+  [n]
+  (if (= 0 n)
+    0
+    (inc (num-digits (quot n 10)))))
+
+(defn seven
+  [m]
+  (loop [m m i 0]
+    (if (<= (num-digits m) 2)
+      [m i]
+      (let [x (quot m 10)
+            y (rem m 10)
+            n (- x (* 2 y))]
+        (recur n (inc i))))))
